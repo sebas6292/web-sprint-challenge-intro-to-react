2f0aea90ca608bf4dead3af50c86a0761868d5b6
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-08-13 14:33:04 -0700

    axios smh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axos'
 
 import characters from './components/Character'
 
@@ -8,9 +9,23 @@ const App = () => {
   // the state properties here...
 const [characters, setCharacters] = useState({})
 
-axios.get('https://swapi.dev/api/people')
-.then(res)
-console.log(res)
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      console.log(res);
+      setCharacters(res.data)
+    })
+      .catch((err) => console.log(err))
+}, [])
+
+const getUpdateCharacters = () => {
+
+  const updateCharacters = characters.map(character => {
+    return character[i];
+  });
+}
+
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -18,6 +33,9 @@ console.log(res)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+        <Characters characters={getUpdateCharacters} />
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+
+import Characters from './character';
+
+export default function Characters(props) {
+    const { characters } = props
+
+    return (
+        <div className='contianer'> 
+
+{ characters.map(character => 
+<Character character={character} key={character.id}/>
+            )}
+        </div>
+    )
+}
