a4804d79e62784036fb298189f8a22eeb44fd931
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-08-13 13:06:03 -0700

    holy grabonzo beans

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import characters from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here...
+const [characters, setCharacters] = useState({})
 
+axios.get('https://swapi.dev/api/people')
+.then(res)
+console.log(res)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
